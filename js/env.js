723bb3cc4921ae2fedb255da3b86724dec3e5165
hao-senbox <mr.hao.anh@senbox.org>
2025-07-09 02:03:23 +0700

    fix

--- js/env.js
+++ js/env.js
@@ -1,9 +1,9 @@
 window.ENV = {
-    API_BASE_URL: 'https://monolith-architect.onrender.com',
+    API_BASE_URL: 'https://flashcard-be-h1bl.onrender.com',
     APP_MODE: 'production'
 };
 
-window.ENV = {
+// window.ENV = {
 //     API_BASE_URL: 'http://localhost:8004',
 //     APP_MODE: 'localhost'
 // }
